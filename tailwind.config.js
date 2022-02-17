8858cd35df7c37b92e91b7c2d8bab8fb136e9fc1
boedegoat <bhremada.fka@gmail.com>
2022-02-17 22:08:51 +0700

    Init commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.ts|tsx'],
+  theme: {
+    extend: {
+      colors: {
+        'dark-gray-100': 'hsl(0, 0%, 55%)',
+        'dark-gray-200': 'hsl(0, 0%, 41%)',
+      },
+    },
+  },
+  plugins: [],
+}
